29667ffb821821b8fc6c96164b6e98e3392afcc5
Timothy Johnson <timothy@wearefound.com>
2013-02-25 12:56:48 -0500

    fix pathing

--- lib/generators/simple_slugs_generator.rb
+++ lib/generators/simple_slugs_generator.rb
@@ -5,7 +5,7 @@ class SimpleSlugsGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   extend ActiveRecord::Generators::Migration
   
-  source_root File.expand_path("../../simple_slug", __FILE__)
+  source_root File.expand_path("../../simple_slugs", __FILE__)
   
   def copy_files(*args)
     migration_template "migration.rb", "db/migrate/create_simple_slugs.rb"
